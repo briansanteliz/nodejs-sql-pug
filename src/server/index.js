687aca4a3fc9cc0920f5327bb0df09f9eb5555c7
Brian <blsvsanteliz@gmail.com>
2020-07-04 14:50:40 -0400

    Add static files and partials layouts

--- src/server/index.js
+++ src/server/index.js
@@ -3,12 +3,19 @@ const path = require('path')
 const routes = require('./routes')
 const app = express()
 
-//settings
+//settings  
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 //middlewares
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, "../public")));
+
+//Muestra el año actual middleware
+app.use((req,res, next)=>{
+    const fecha = new Date()
+    res.locals.fecha = fecha.getFullYear()
+    next()
+})
 
 app.listen(4000)
 app.use(routes)
